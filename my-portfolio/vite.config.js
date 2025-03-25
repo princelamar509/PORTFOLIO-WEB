89024b53912c6e265990722184dd781b23801721
JAMES M <168496401+princelamar509@users.noreply.github.com>
2025-03-25 00:38:56 -0400

    ghp

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from "vite-plugin-gh-pages"
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base : '/PORTFOLIO-WEB/'
 })
